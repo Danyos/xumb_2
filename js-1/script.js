c17f519a29e80ba04649ebd3aba31d5323de4811
Danyos <daniel.hambardzumyan@mail.ru>
2022-12-19 18:56:40 +0400

    Javascript popoxakaner

---
+++ js-1/script.js
@@ -0,0 +1,56 @@
+// 3
+// document.write(window)
+// alert(window)
+
+
+
+
+// console.log(((5+5*2-2-1)/6)**4)
+
+// let x=7
+// let y=3
+// x=x+y
+// console.log(x)
+//  x=458
+// console.log(x)
+//
+//
+//
+//
+// let UserName
+// let user_name1
+// // let user-name
+//
+//
+// let num=54
+// let text="barev vonces Ara jan"
+//
+//
+// let t$t
+// t$t=text
+// console.log(t$t)
+
+// tnayin
+// nayel
+// https://learn.javascript.ru/variables
+// https://learn.javascript.ru/alert-prompt-confirm
+// https://learn.javascript.ru/operators
+// grel
+
+// 1. x ev y textery poxel hakarak
+// let x=8
+// let y=14
+// let z=0
+
+
+
+
+// consol.log(x)
+// consol.log(y)
+
+// 2// tvery shur tal hakarak 
+// let x=8
+// let y=14
+// ........
+// consol.log(x)//14
+// consol.log(y)//8
