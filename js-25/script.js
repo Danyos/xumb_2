3ae91f5f721e3e7e1f6ce3abf857b63d4faeed5b
Danyos <daniel.hambardzumyan@mail.ru>
2023-03-06 18:08:06 +0400

    object

--- js-25/script.js
+++ js-25/script.js
@@ -29,29 +29,29 @@
 // ]
 
 
-let infos = {
-    id: 1,
-    name: "Inga",
-    status: true,
-    images: [1, 2, 3, 4, 5, 6],
-    info2 : {
-        name: "Inga",
-        country: "Yerevan",
-        from: "Bangladesh",
-        phoneNumber: "123456789"
-    }
-
-}
-
-
-let x = Object.keys(infos.info2)
-// //
-for (let i=0;i<x.length;i++){
-infos[infos.info2[x[i]]]=i
-    // console.log(x[i],)
-}
-
-console.log(infos)
+// let infos = {
+//     id: 1,
+//     name: "Inga",
+//     status: true,
+//     images: [1, 2, 3, 4, 5, 6],
+//     info2 : {
+//         name: "Inga",
+//         country: "Yerevan",
+//         from: "Bangladesh",
+//         phoneNumber: "123456789"
+//     }
+//
+// }
+//
+//
+// let x = Object.keys(infos.info2)
+// // //
+// for (let i=0;i<x.length;i++){
+// infos[infos.info2[x[i]]]=i
+//     // console.log(x[i],)
+// }
+//
+// console.log(infos)
 //
 // let arr=[10,20,30,40,50,60,70]
 //
@@ -96,52 +96,50 @@ console.log(infos)
 //     console.log(infosKey,infos.info2[infosKey])
 // }
 
-// let infoCart=[
-//     {
-//         id:1,
-//         productName:"item-1",
-//         price:"700$"
-//     },
-//     {
-//         id:2,
-//         productName:"item-2",
-//         price:"800$"
-//     },
-//     {
-//         id:3,
-//         productName:"item-3",
-//         price:"450$"
-//     },
-//     {
-//         id:4,
-//         productName:"item-4",
-//         price:"300$"
-//     },
-//     {
-//         id:5,
-//         productName:"item-5",
-//         price:"200$"
-//     },
-//     {
-//         id:6,
-//         productName:"item-6",
-//         price:"250$"
-//     }
-// ]
+let infoCart=[
+    {
+        id:1,
+        productName:"item-1",
+        price:"700$"
+    },
+    {
+        id:2,
+        productName:"item-2",
+        price:"800$"
+    },
+    {
+        id:3,
+        productName:"item-3",
+        price:"450$"
+    },
+    {
+        id:4,
+        productName:"item-4",
+        price:"300$"
+    },
+    {
+        id:5,
+        productName:"item-5",
+        price:"200$"
+    },
+    {
+        id:6,
+        productName:"item-6",
+        price:"250$"
+    }
+]
 //
 // let arrinfo=["250$","400$","200$","300$","450$","800$","700$"]
-//
-// let max={
-//     price:0
-// }
-// for (const infoValue of infoCart) {
-//     if( parseFloat(infoValue.price)>parseFloat(max.price)){
-//         max=infoValue
-//
-//     }
-// }
-//
-// console.log(max)
+
+let max=infoCart[0]
+for (const infoValue of infoCart) {
+    if( parseFloat(max.price)>parseFloat(infoValue.price)){
+        max=infoValue
+
+    }
+}
+
+console.log(max)
 
 
 
