31cb50dde822eb62c4c785c08ec78fb8f9e1bf74
Danyos <daniel.hambardzumyan@mail.ru>
2023-03-10 16:23:58 +0400

    obj

---
+++ js-26/script.js
@@ -0,0 +1,136 @@
+const patmentList = [
+    {
+        id: 1,
+        name: "Valod",
+        payment_way: "visa",
+        exchange: 'usd',
+        price: "1500$"
+    },
+    {
+        id: 2,
+        name: "Vazgen",
+        payment_way: "paypal",
+        exchange: 'usd',
+        price: "1400$"
+    },
+    {
+        id: 3,
+        name: "grish",
+        payment_way: "bitcoin",
+        exchange: 'usd',
+        price: "1700$"
+    },
+    {
+        id: 4,
+        name: "Karo",
+        payment_way: "visa",
+        exchange: 'usd',
+        price: "100$"
+    },
+    {
+        id: 5,
+        name: "Serob",
+        payment_way: "bitcoin",
+        exchange: 'usd',
+        price: "500$"
+    },
+    {
+        id: 6,
+        name: "Valod",
+        payment_way: "visa",
+        exchange: 'eur',
+        price: "1500e"
+    },
+    {
+        id: 7,
+        name: "Vazgen",
+        payment_way: "paypal",
+        exchange: 'eur',
+        price: "1400e"
+    },
+    {
+        id: 8,
+        name: "grish",
+        payment_way: "bitcoin",
+        exchange: 'eur',
+        price: "1700e"
+    },
+    {
+        id: 9,
+        name: "Karo",
+        payment_way: "visa",
+        exchange: 'amd',
+        price: "100d"
+    },
+    {
+        id: 10,
+        name: "Serob",
+        payment_way: "bitcoin",
+        exchange: 'amd',
+        price: "500d"
+    },
+    {
+        id: 11,
+        name: "Valod",
+        payment_way: "visa",
+        exchange: 'amd',
+        price: "1500d"
+    },
+    {
+        id: 12,
+        name: "Vazgen",
+        payment_way: "paypal",
+        exchange: 'amd',
+        price: "1400d"
+    },
+    {
+        id: 13,
+        name: "grish",
+        payment_way: "bitcoin",
+        exchange: 'rub',
+        price: "1700r"
+    },
+    {
+        id: 14,
+        name: "Karo",
+        payment_way: "visa",
+        exchange: 'rub',
+        price: "100r"
+    },
+    {
+        id: 15,
+        name: "Serob",
+        payment_way: "bitcoin",
+        exchange: 'rub',
+        price: "500r"
+    },
+    {
+        id: 16,
+        name: "Serob",
+        payment_way: "bitcoin",
+        exchange: 'rub',
+        price: "500r"
+    },
+
+]
+
+let pay = {
+    bitcoin: {
+        amd: 1000,
+        rub: 300,
+        eur: 1802,
+        usd: 700
+    },
+    visa: {
+        amd: 1000,
+        rub: 300,
+        eur: 1802,
+        usd: 700
+    },
+    paypal: {
+        amd: 1000,
+        rub: 300,
+        eur: 1802,
+        usd: 700
+    }
+}
